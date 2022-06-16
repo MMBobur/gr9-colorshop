268b6f358695b9aa5f78f2dc147505fb4cd6aabd
Umidjon <umidjonshodiyorov@gmail.com>
2022-06-15 23:11:44 -0700

    'banner_lates'

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -1,9 +1,28 @@
-
-function Banner() {
+import { Button, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+import Body from '../../assets/black_woman.jpg'
+import './banner.css'
+const Banner = () => {
   return (
-    <>
-      Banner
-    </>
+    <Box className="more" sx={{color:'black'}}>
+      <Typography sx={{pt:'15%',pl:'10%'}}>
+
+      <Typography>
+        SPRING / SUMMER COLLECTION 2017
+      </Typography>
+      <Typography variant='h2' sx={{fontSize:'75px',opacity:'0.8'}}>
+       Get up to 30% Off
+      </Typography>
+      <Typography variant="h2" sx={{fontSize:'75px',opacity:'0.8'}}>
+        New Arrivals  
+      </Typography>
+      <br /><br />
+      <Button sx={{color:'white',border:'1px solid #FE4C50',bgcolor:'#FE4C50',width:'10%'}}>
+        Shop now
+      </Button>
+      </Typography>
+    </Box>
   )
 }
 
